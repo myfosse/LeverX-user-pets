454daf774f3dd9bb777b9bcf17008a258a52b455
andrei.yahorau <andrei.yahorau@leverx.com>
2021-02-17 12:54:51 +0300

    IEMEREDU13-60(user-pets): Initial commit

---
+++ src/main/java/com/leverx/UserPetsApplication.java
@@ -0,0 +1,13 @@
+package com.leverx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPetsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserPetsApplication.class, args);
+  }
+
+}
