1bcf62a603d363c0efd4d01ddb2fb924bfed7ac6
andrei.yahorau <andrei.yahorau@leverx.com>
2021-02-24 17:57:16 +0300

    IEMEREDU13-60(user-pets): Code fix after review

--- src/main/java/com/leverx/constant/controller/ControllerConstant.java
+++ src/main/java/com/leverx/constant/controller/ControllerConstant.java
@@ -11,7 +11,7 @@ public final class ControllerConstant {
   public static final String PREFIX = "/api/v1";
 
   public static final String ENDPOINT_AUTH  = PREFIX + "/auth";
-  public static final String ENDPOINT_CATS  = PREFIX + "/cats/";
+  public static final String ENDPOINT_CATS  = PREFIX + "/cats";
   public static final String ENDPOINT_DOGS  = PREFIX + "/dogs";
   public static final String ENDPOINT_PETS  = PREFIX + "/pets";
   public static final String ENDPOINT_USERS = PREFIX + "/users";
